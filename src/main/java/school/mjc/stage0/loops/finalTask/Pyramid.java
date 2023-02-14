3f8d9d059c14bed9c35dd63ca7d8e56224c2ae7d
jasurrahmon1994 <jasurrahmon24@gmail.com>
2023-02-14 14:36:08 +0500

    first try

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+        }
     }
 
     public static void main(String[] args) {
